81c2b7ef035847ad8e9f90aebe6bcfa203415b58
Sajid Zaman <zsajid014@gmail.com>
2018-07-09 03:05:04 +0000

    Classwork 5

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs');
+  var server = http.createServer(function(req, res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('index.html', function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+       res.end(data);
+    });
+  });
+  server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+  });
