33bb3b9042e8194454c4e9a3e0d4163d4eefe6eb
Sajid Zaman <zsajid014@gmail.com>
2018-07-11 03:11:15 +0000

    Classwork 6

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var http = require('http');
-var fs = require('fs');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html', function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-       res.end(data);
-    });
-  });
+var express = require('express');
+var app = express();
+var server = http.Server(app);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname+'/index.html');
+});
+app.get('/about', function(req, res) 
+  {
+    res.sendFile(__dirname+'/about.html');
+  
+});
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
